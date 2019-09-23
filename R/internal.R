4f40fc170e54ca9744ded8fb163076fb0641ca3e
stla <laurent_step@yahoo.fr>
2019-09-23 20:34:54 +0200

    removed isNotNegativeInteger

--- R/internal.R
+++ R/internal.R
@@ -7,10 +7,6 @@ isPositiveInteger <- function(m){
   is.vector(m) && is.numeric(m) && length(m) == 1L && floor(m) == m
 }
 
-isNotNegativeInteger <- function(z){
-  Im(z) != 0 || Re(z)>0 || Re(z) != trunc(Re(z))
-}
-
 isSymmetricPositive <- function(M){
   isSymmetric(M) && all(eigen(M, symmetric = TRUE, only.values = TRUE)$values >= 0)
 }
